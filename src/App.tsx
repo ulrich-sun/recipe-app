5a744cc437df43f95522adb7cd30cf8c3c187e75
armandnjapou <armand.njapoungassu@consulting-for.accor.com>
2025-04-16 20:00:32 +0200

    add recipe app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import NavBar from "./components/ui/NavBar.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import LoginForm from "./components/auth/LoginForm.tsx";
+import RegisterForm from "./components/auth/RegisterForm.tsx";
+import ProtectedRoute from "./components/ui/ProtectedRoute.tsx";
+import RecipeList from "./components/recipes/RecipeList.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <div className="min-h-screen bg-gray-100">
+                <NavBar/>
+                <main className="container mx-auto px-4 py-8">
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/login" element={<LoginForm/>}/>
+                        <Route path="/register" element={<RegisterForm/>}/>
+                        <Route
+                            path="/recipes"
+                            element={
+                                <ProtectedRoute>
+                                  <RecipeList />
+                                </ProtectedRoute>
+                            }/>
+                      <Route path="*" element={<Navigate to="/" replace /> } />
+                    </Routes>
+                </main>
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ src/services/api.ts
@@ -0,0 +1,115 @@
+import axios from "axios";
+
+const API_URL = 'https://localhost:8443/api';
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        "Content-Type": "application/json",
+    }
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+)
+
+export interface Recipe {
+    id: number;
+    title: string;
+    description: string;
+    ingredients: string;
+    cuisine: string;
+    cookingTime: number;
+}
+
+export interface UserRequest {
+    username: string;
+    password: string;
+}
+
+export interface PageableObject {
+    offset: number;
+    pageNumber: number;
+    pageSize: number;
+    paged: boolean;
+    unpaged: boolean;
+}
+
+export interface SortObject {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+}
+
+export interface PageRecipe {
+    content: Recipe[];
+    totalElements: number;
+    totalPages: number;
+    size: number;
+    number: number;
+    numberOfElements: number;
+    first: boolean;
+    last: boolean;
+    empty: boolean;
+    pageable: PageableObject;
+    sort: SortObject;
+}
+
+export interface RecipeFilters {
+    cuisine?: string;
+    title?: string;
+    ingredients?: string;
+    maxCookingTime?: number;
+}
+
+export const AuthService = {
+    register: async (userData: UserRequest): Promise<any> => {
+        const response = await api.post(`${API_URL}/auth/register`, userData);
+        return response.data;
+    },
+
+    login: async (userData: UserRequest): Promise<any> => {
+        const response = await api.post(`${API_URL}/auth/login`, userData);
+        const { token } = response.data;
+
+        if (token) {
+            localStorage.setItem('token', token);
+        }
+
+        return response.data;
+    },
+
+    logout: (): void => {
+        localStorage.removeItem('token');
+    },
+
+    isAuthenticated: (): boolean => {
+        return !!localStorage.getItem('token');
+    }
+};
+
+export const RecipeService = {
+    getRecipes: async (pageNumber= 0, pageSize = 10): Promise<PageRecipe> => {
+        const response = await api.get(`${API_URL}/recipes`, {
+            params: {pageNumber, pageSize}
+        });
+        return response.data;
+    },
+
+    getFilterRecipes: async(filters: RecipeFilters): Promise<Recipe[]> => {
+        const response = await api.get(`${API_URL}/recipes/filter`, {
+            params: filters
+        });
+
+        return response.data;
+    }
+}
+
+export default api;
