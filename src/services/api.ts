a0b15bfb94e06c100705d7fbea082b58bdad5c8c
Ubuntu <ubuntu@ip-172-31-92-74.ec2.internal>
2025-06-05 10:26:40 +0000

    test

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-#const API_URL = 'https://localhost:8443/api';
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8443/api';
 
 const api = axios.create({
