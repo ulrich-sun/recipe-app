745ae8c762075f1822f4fa82e927f091111c1fd5
Ubuntu <ubuntu@ip-172-31-89-245.ec2.internal>
2025-06-13 08:56:35 +0000

    config.json

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { initApiBaseUrl } from './services/api'   // <-- chemin corrigé
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// On attend que l'API_URL soit chargée avant de lancer l'application
+initApiBaseUrl().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+})
